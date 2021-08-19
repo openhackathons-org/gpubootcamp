88989394d8cf9fad1104f34901f1bf4fc2ca6f98
Mozhgan K. Chimeh <mozhgan.kabiri@gmail.com>
2021-08-19 18:14:03 +0100

    bootcamp template v2

---
+++ misc/Bootcamp_template/appName/appName_labs/English/C/source_code/lab1/SOLUTION/hello_world.cpp
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// if the code is for another author, give credit as below. Otherwise only add the Copyright piece
+// Author: xx and yy  , organization
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// Copyright (c) 2021 NVIDIA Corporation.  All rights reserved.
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
